8fe4ec51e10d9e9c5a5931608a1d2918bcb6d62b
bitemily <420826029@qq.com>
2021-03-30 23:10:44 +0800

    update

--- test_btree.py
+++ test_btree.py
@@ -5,28 +5,31 @@ def run_test_btree_insert():
     import random
     a = set([random.randrange(0,300,1) for i in range(10)])
     print(a)
-    # a = {256, 3, 106, 267, 143, 146, 281, 190, 127}
+    # a = {33, 193, 166, 232, 298, 235, 149, 89, 61}
+    # a = [3,1,5,4,2,9,10,8,7,6]
+    # a = {192, 32, 162, 256, 195, 69, 234, 236, 44, 79, 176, 113, 215, 152, 281, 218, 187, 221, 126}
 
     # test insert
     for i in a:
+        print(i)
         btree.insert(i)
     btree.print_tree()
     print("\n")
 
     # test min max find
-    print(btree.min())
-    print(btree.max())
-    print(btree.find(btree.min()))
-    print(btree.find(320))
-    print("\n")
+    # print(btree.min())
+    # print(btree.max())
+    # print(btree.find(btree.min()))
+    # print(btree.find(320))
+    # print("\n")
 
     # test delete
-    # for i in a:
-    #     print("to delete: {}".format(i))
-    #     btree.delete(i)
+    for i in a:
+        print("to delete: {}".format(i))
+        btree.delete(i)
 
-    # print("\n")
-    # btree.print_tree()
+    print("\n")
+    btree.print_tree()
     
 
 if __name__ == '__main__':
